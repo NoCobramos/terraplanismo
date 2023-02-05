93acbad819abd863b98daad1856f456aeec4077e
Cristo Doniz <cristojosedm@gmail.com>
2023-02-05 16:58:33 +0000

    CSharp Strings 4-5-6(6 unfinished)

--- practica 5/C#/solucion/EjerciciosC/CristoCSharpStrings4-5-6-7/CristoCSharpString6/Program.cs
+++ practica 5/C#/solucion/EjerciciosC/CristoCSharpStrings4-5-6-7/CristoCSharpString6/Program.cs
@@ -7,9 +7,30 @@ namespace String2
     {
         static void Main(string[] args)
         {
+            char char1;
+            string cadenaFin = "";
             Console.WriteLine("Introduce una cadena por favor: ");
-            string cadena = Console.ReadLine();
-            
+            string cadena1 = Console.ReadLine();
+            Console.WriteLine("Introduce la segunda cadena por favor: ");
+            string cadena2 = Console.ReadLine();
+            if (cadena1.Length > cadena2.Length)
+            {
+                for(int i = 0; i< cadena1.Length; i++)
+                {
+                    if (cadena2[i] == cadena1[i])
+                    {
+                        char1 = cadena1[i];
+                        cadenaFin = char1.ToString();
+                    }
+
+                }
+                
+                Console.WriteLine(cadenaFin);
+            }
+            if(cadena1.Length<cadena2.Length)
+            {
+
+            }
 
 
         }
