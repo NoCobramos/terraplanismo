a803c8961ca733e8f271e14b96aeea6caa40f6a4
Cristo Doniz <cristojosedm@gmail.com>
2023-02-05 15:54:55 +0000

    CSharp Strings 4-5
    6 unfinished

---
+++ practica 5/C#/solucion/EjerciciosC/CristoCSharpStrings4-5-6-7/CristoCSharpString6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace String2
+{
+    class String02
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce una cadena por favor: ");
+            string cadena = Console.ReadLine();
+            
+
+
+        }
+    }
+}
