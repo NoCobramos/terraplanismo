33c12d660cc56f71bf60e92cf494815e362ee0f0
endika pradera touzani <endikadawb@gmail.com>
2023-02-06 11:29:49 +0000

    EJERCICIOS NUM
    ULTIMOS 3 EJERCICIOS DE C# DEL APARTADO NUM

---
+++ practica 5/C#/solucion/EjerciciosC/EJER_NUM_ENDIKA_C#/EJER_NUMEROS_ENDIKA/Funciones.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJER_NUMEROS_ENDIKA
+{
+    internal class Funciones
+    {
+        //Funciones
+        public static void SecuenciaPell()
+        {
+            int number1 = 0, number2 = 1;
+            int aux = 0;
+
+            //Casos bases 
+            /*Console.WriteLine(number1);
+            Console.WriteLine(number2);*/
+
+            for(int i  = 0; i < 15; i++) 
+            { 
+                aux = (number2 * 2) + number1;
+                Console.WriteLine(aux);
+                number1 = number2;
+                number2 = aux;
+            }
+        }
+
+        public static bool Armstrong(string number)
+        {
+            int num_usu = Convert.ToInt32(number);
+            int longitud = number.Length;
+            double resultado = 0;
+            
+            for(int i = 0; i < longitud; i++)
+            {
+                resultado += Math.Pow(Convert.ToInt32(number[0]),longitud);
+            }
+
+            if(resultado == num_usu)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public static bool NumerosDif(int number)
+        {
+            string str = number.ToString();
+            return str.Length == str.Distinct().Count();
+        }
+
+
+        //Para finalizar el progrma
+        public static void Bye()
+        {
+            Console.WriteLine("Pulser cualquier tecla para finalizar...");
+            Console.ReadKey();
+        }
+    }
+}
